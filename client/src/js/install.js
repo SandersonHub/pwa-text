67d367143e4b890f02d6270a6465f1a33cd7aede
SandersonHub <128574459+SandersonHub@users.noreply.github.com>
2023-09-19 19:53:38 -0400

    updated install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,32 +1,27 @@
-const butInstall = document.getElementById("buttonInstall");
+const butInstall = document.getElementById('buttonInstall');
 
+// Logic for installing the PWA
+// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
 
-    // Store the triggered events
-    window.deferredPrompt = event;
-
-    // Remove the hidden class from the button.
-    butInstall.classList.toggle('hidden', false);
-  });
-
-butInstall.addEventListener('click', async () => {
-
-  const promptEvent = window.deferredPrompt;
-
-  if (!promptEvent) {
-   return;
-  }
-
-  // Show prompt
-  promptEvent.prompt();
-
-  // Reset the deferred prompt variable, it can only be used once.
-  window.deferredPrompt = null;
-
-  butInstall.classList.toggle('hidden', true);
+  //callback function
+event.preventDefault();
+// Stash the event so it can be triggered later.
+butInstall.style.visibility = 'visible';
+// Update the install UI, tells the user if the its actually installed or not.
+butInstall.textContent = 'app should be installed'
 });
 
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+
+//sets the disabled attribute to true
+//when the button is clicked it won't be able to be clicked again
+butInstall.setAttribute('disabled', true);
+butInstall.textContent = 'installing app...I think?';
+});
+
+// TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-  // Clear prompt
-  window.deferredPrompt = null;
+  console.log('installed', event);
 });
